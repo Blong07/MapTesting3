4821e0fcb800f9a17c97e3a7f37fd380e2bc7487
Alex Blong <alexanderblong07@gmail.com>
2024-09-16 19:02:34 +1200

    Initial Commit

---
+++ MapTesting3/SearchHandler.swift
@@ -0,0 +1,18 @@
+//
+//  SearchHandler.swift
+//  MapTesting3
+//
+//  Created by Alexander Blong on 16/09/2024.
+//
+
+import SwiftUI
+
+struct SearchHandler: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchHandler()
+}
---
+++ MapTesting3/SearchOverlay.swift
@@ -0,0 +1,18 @@
+//
+//  SearchOverlay.swift
+//  MapTesting3
+//
+//  Created by Alexander Blong on 16/09/2024.
+//
+
+import SwiftUI
+
+struct SearchOverlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchOverlay()
+}
