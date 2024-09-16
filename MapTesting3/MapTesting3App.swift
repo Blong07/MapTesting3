5330daa9353c724c17ce35f3e29d6df3833aa222
Blon g07 <alexanderblong07@gmail.com>
2024-09-16 19:02:34 +1200

    Initial Commit

---
+++ MapTesting3/MapTesting3App.swift
@@ -0,0 +1,33 @@
+//
+//  MapTesting3App.swift
+//  MapTesting3
+//
+//  Created by Alexander Blong on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapTesting3App: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
---
+++ MapTesting3Tests/MapTesting3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MapTesting3Tests.swift
+//  MapTesting3Tests
+//
+//  Created by Alexander Blong on 16/09/2024.
+//
+
+import Testing
+@testable import MapTesting3
+
+struct MapTesting3Tests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
